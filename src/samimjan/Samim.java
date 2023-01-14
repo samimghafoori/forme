f30f1b59baf1c7d034581843d545113f4de2cb97
samimghafoori <samimghafoori@yahoo.com>
2023-01-13 19:18:07 -0800

    fourth commit

--- src/samimjan/Samim.java
+++ src/samimjan/Samim.java
@@ -5,6 +5,7 @@ public class Samim {
 	public static void main(String[] args) {
 	
 		System.out.println("this is from eclipes think of it");
+		System.out.println("this is second change we made by this");
 
 	}
 
